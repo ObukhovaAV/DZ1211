87f0f88fe76e4463ffcf25ee7707e54b3a45d8f8
Anastasya <okayota83@mail.ru>
2022-11-13 18:32:10 +0400

    add Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+void ShowNumbersRec(int M, int N)
+{
+    if (M<=N)
+    {
+    Console.Write($"{M} ");
+    ShowNumbersRec(M+1, N);
+    }
+}
+
+Console.WriteLine("Введите число M");
+int M = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine() ?? "0");
+if (M>0 && N>0)
+ShowNumbersRec(M,N);

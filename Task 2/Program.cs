5ae01378e007e60d3b28e352f23e30993e3976f5
Anastasya <okayota83@mail.ru>
2022-11-13 19:27:46 +0400

    add Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SumNumbersRec(int M, int N)
+{
+    if (M==N) return N;
+    else return M + SumNumbersRec (M+1, N);    
+  
+}
+
+Console.WriteLine ("Введите М");
+int M = int.Parse (Console.ReadLine() ?? "0");
+Console.WriteLine ("Введите N");
+int N = int.Parse (Console.ReadLine() ?? "0");
+
+int sum = SumNumbersRec (M,N);
+Console.WriteLine ($"Сумма элементов от M до N = {sum}");

0d3988ca30f698fa8db339d61da5787a0d73e7c3
Anastasya <okayota83@mail.ru>
2022-11-18 10:02:39 +0400

    add Task 4

--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -2,21 +2,25 @@
 //для которых каждое следующее равно сумме двух предыдущих. 
 //Первые два элемента последовательности задаются пользователем
 
-int Sum (int n, int m, int c)
+
+void SumNumber(int first, int second, int n)
 {
-    int sum = n + m;
-    if (c==0) return 1;
-    else return Sum ()
+  int t = 0;
+  if (n > 0)
+    {
+      Console.Write(first + second + " ");
+      t = first;
+      first = second;
+      second = second + t;
+      SumNumber(first, second, n - 1);
+    }
 }
 
-Console.WriteLine("Введите первое число ");
-int first = int.Parse(Console.ReadLine() ?? "0");
-Console.WriteLine("Введите второе  число ");
-int second = int.Parse(Console.ReadLine() ?? "0");
-Console.WriteLine("Введите количество  чисел ");
-int count = int.Parse (Console.ReadLine() ?? "0");
-
-for (int i = first; i < count; i++)
-{
-  Console.WriteLine(Sum(i));
-}
+Console.WriteLine ("Введите первое число");
+int first = int.Parse (Console.ReadLine() ?? "0");
+Console.WriteLine ("Введите второе  число");
+int second = int.Parse (Console.ReadLine() ?? "0");
+Console.WriteLine ("Введите количество чисел");
+int n = int.Parse (Console.ReadLine() ?? "0");
+Console.Write ($"{first} {second} ");
+SumNumber(first, second, n-2);

16fe166142bb8593c71519fc30a415e5c98e08c2
Anastasya <okayota83@mail.ru>
2022-11-18 08:41:43 +0400

    add Task 3

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -1,2 +1,15 @@
 ﻿// Написать программу вычисления функции Аккермана
-Console.WriteLine("Hello, World!");
+
+int Ackermann (int m, int n)
+{
+    if (m==0) return n+1;
+    if (m>0 && n==0) return Ackermann (m-1, 1);
+    if (m>0 && n>0) return Ackermann ((m-1), Ackermann (m,n-1));
+    return Ackermann (m,n);
+}
+Console.WriteLine ("Введите число m");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine ("Введите число n");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int f = Ackermann (m,n);
+Console.WriteLine ($"Функция Аккермана для чисел {m} и {n} равна {f}");
--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -1,4 +1,22 @@
-﻿// Написать программу показывающие первые N чисел, 
+﻿// Написать программу, показывающую первые N чисел, 
 //для которых каждое следующее равно сумме двух предыдущих. 
 //Первые два элемента последовательности задаются пользователем
 
+int Sum (int n, int m, int c)
+{
+    int sum = n + m;
+    if (c==0) return 1;
+    else return Sum ()
+}
+
+Console.WriteLine("Введите первое число ");
+int first = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите второе  число ");
+int second = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите количество  чисел ");
+int count = int.Parse (Console.ReadLine() ?? "0");
+
+for (int i = first; i < count; i++)
+{
+  Console.WriteLine(Sum(i));
+}
